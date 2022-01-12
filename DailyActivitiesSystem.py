afb6449734e2f2949c1adeda8876fda877fb5fc1
AndersLindh1 <54248025+AndersLindh1@users.noreply.github.com>
2022-01-12 18:01:01 +0100

    Update DailyActivitiesSystem.py

--- DailyActivitiesSystem.py
+++ DailyActivitiesSystem.py
@@ -3,7 +3,7 @@
 
 import argparse
 
-paser = argparse.ArgumentParser(description='Support system to execute daily activities')
+parser = argparse.ArgumentParser(description='Support system to execute daily activities')
 parser.add_argument('list', help='List remaining activities for today')
 parser.add_argument('all', help='Show all activities for today')
 parser.print_help()                    

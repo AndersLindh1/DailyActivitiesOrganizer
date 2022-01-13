3396ecb0ffc796a6ceaba4b3eb84955b17d2682b
AndersLindh1 <anderspunktlindh@gmail.com>
2022-01-13 05:58:46 +0100

    add print_help again

--- DailyActivitiesSystem.py
+++ DailyActivitiesSystem.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Support system to execute daily activities')
 parser.add_argument('list', help='List remaining activities for today')
 parser.add_argument('all', help='Show all activities for today')
-#parser.print_help()                    
+parser.print_help()                    
 
 print("testing")
 print("new line")

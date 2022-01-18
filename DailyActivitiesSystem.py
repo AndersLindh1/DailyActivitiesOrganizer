d3d9a27d3e30c1fa4a2dc92b777a88ac9b77adf0
AndersLindh1 <anderspunktlindh@gmail.com>
2022-01-18 22:07:02 +0100

    data structure changed. The argument list is corrected, others not yet

--- DailyActivitiesSystem.py
+++ DailyActivitiesSystem.py
@@ -6,12 +6,13 @@ import argparse
 def getActivities(file=None):
 	if file == None:
 		print("no filename provided, use default")
-		activities={"Mindfulness": [10,11], "Read": [15, 7], "Pybites": [15, 0], "workout": [10, 0]}
+		activities_list=[{"Event": "Mindfulness", "Time_planned": 10, "Time_done": 10, "Order": 1, "Periodicity": 1, "Weekdays": 0, "Day_counter": 0}, {"Event": "Read", "Time_planned":15, "Time_done": 0, "Order": 0, "Periodicity": 2, "Weekdays": 0, "Day_counter": 0}]
+#		activities={"Mindfulness": [10,11], "Read": [15, 7], "Pybites": [15, 0], "workout": [10, 0]}
 	else:
 		print("file provided")
-	return activities
+	return activities_list
 
-activities = getActivities()
+activities_list = getActivities()
 
 parser = argparse.ArgumentParser(description='Support system to execute daily activities')
 parser.add_argument("-l", "--list", action='store_true', help='List remaining activities for today')
@@ -24,9 +25,15 @@ parser.print_help()  # debug
 args = parser.parse_args()
 if args.list:
 	print("List selected")
-	for act,time in activities.items():
-		if time[1]<time[0]:
-			print(act, time[0]-time[1], "minutes left")
+#	for act,time in activities.items():
+	for act in activities_list:
+		# if time[1]<time[0]:
+		#print(act)
+	#	print(act.items())
+		for key, value in act.items():
+				print(key + ": " + str(value))
+#		print(act)
+#			print(act, time[0]-time[1], "minutes left")
 if args.today:
 	print("show todays activities, regardless their status")
 	print(activities)
@@ -54,7 +61,7 @@ if args.time:
 		print(activities[args.time[0]])
 		activities[args.time[0]][1]=activities[args.time[0]][1] + int(args.time[1])
 
-print(activities)
+#print(activities_list)
 
 #print("testing")
 #print("new line")
